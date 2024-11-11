7ccf98db903862fb7273d15d46893e968ec798b2
serejina <serejinam@gmail.com>
2024-11-11 10:18:37 +0100

    Update pid_controller.cpp

--- control/pid_controller.cpp
+++ control/pid_controller.cpp
@@ -51,7 +51,8 @@ void PID::UpdateError(double cte) {
 
    this->d_error = this->delta_time > 0 ? (cte - this->p_error)/this->delta_time : 0.0;
    
-   this->i_error += cte * this->delta_time; 
+   this->i_error += cte * this->delta_time;
+   std::cout << "this->delta_time: " << this->delta_time << std::endl; 
 }
 
 double PID::TotalError() {
@@ -59,7 +60,7 @@ double PID::TotalError() {
    * TODO: Calculate and return the total error
     * The code should return a value in the interval [output_lim_mini, output_lim_maxi]
    */
-    double control = this->p_k * this->p_error + this->d_k * this->d_error + this->i_k * this->i_error;
+    double control = (this->p_k * this->p_error) + (this->d_k * this->d_error) + (this->i_k * this->i_error);
     return clamp(control, this->output_lim_min, this->output_lim_max);
 }
 
@@ -68,5 +69,6 @@ double PID::UpdateDeltaTime(double new_delta_time) {
    * TODO: Update the delta time with new value
    */
    this->delta_time = new_delta_time;
+   std::cout <<"this->delta_time: " << this->delta_time << std::endl;
    return this->delta_time;
 }
